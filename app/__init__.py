fa9a6a79d5a1b10e84c5533f2f8433a34b037ad2
VikasYadav2005 <vikas.cy@somaiya.edu>
2025-04-17 12:03:18 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Welcome to the Python Azure CI/CD demo!"
-
-@app.route("/add")
-def add():
-    try:
-        a = float(request.args.get("a", 0))
-        b = float(request.args.get("b", 0))
-        return jsonify({"result": a + b})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
+from app import routes
