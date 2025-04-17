0db0138675d33d84727da0a013b523191bd4c523
VikasYadav2005 <vikas.cy@somaiya.edu>
2025-04-17 12:02:59 +0530

    Create routes.py

---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from app import app
+from flask import jsonify, request
+
+@app.route("/")
+def home():
+    return "Welcome to the Python Azure CI/CD demo!"
+
+@app.route("/add")
+def add():
+    try:
+        a = float(request.args.get("a", 0))
+        b = float(request.args.get("b", 0))
+        return jsonify({"result": a + b})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
